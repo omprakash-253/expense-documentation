fbb702dda8fe9d205193c92c32cf1f0cd2892b26
omprakash-253 <omprakashdevops253@gmail.com>
2025-07-08 13:39:59 +0530

    mysql.sh code updated

--- mysql.sh
+++ mysql.sh
@@ -34,17 +34,17 @@ fi
 dnf install mysql-server -y &>>$logfile
 VALIDATE $? " Installation of mysql server"
 
-systemctl enable mysql-d $>>$logfile
+systemctl enable mysql-d &>>$logfile
 VALIDATE $? "enabling of mysql server"
 
-systemctl start mysql-d $>>$logfile
+systemctl start mysql-d &>>$logfile
 VALIDATE $? "starting of mysql server"
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
 # VALIDATE $? "Setting up root password"
 
-#Below code will be useful fir idempotent nature (replace 43 & 44 line)
-mysql -h localhost -u root -p ${mysql_root_password} -e 'show databases;' &>>$logfile
+#Below code will be useful for idempotent nature (replace 43 & 44 line)
+mysql -h db.omansh.fun -u root -p ${mysql_root_password} -e 'show databases;' &>>$logfile
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$logfile
